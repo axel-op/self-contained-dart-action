029f294447a97a874e551d6f2896ad17fd190b11
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2022-02-20 15:28:42 +0100

    Set an output

--- app/bin/main.dart
+++ app/bin/main.dart
@@ -1,7 +1,13 @@
 import 'package:github_actions_toolkit/github_actions_toolkit.dart' as gaction;
 
 void main() {
+  // read the input
   const input = gaction.Input("who-to-greet", isRequired: false);
   final whoToGreet = input.value ?? "world";
+
+  // print the message
   gaction.log.info('Hello $whoToGreet!');
+
+  // set the output
+  gaction.setOutput("time", DateTime.now().toString());
 }
